45b50e74a1060456d475840268e2b072ff973678
Helmanz <helcze4@gmail.com>
2024-10-15 16:18:33 +0200

    removed debug closest target

--- Assets/Scripts/ModelManager.cs
+++ Assets/Scripts/ModelManager.cs
@@ -54,8 +54,6 @@ public class ModelManager : MonoBehaviour
                 mat = Undefined;
             }
             child.GetComponent<MeshRenderer>().material = mat;
-            var closest = child.AddComponent<closestgobj>();
-            closest.closest = gameojb;
         });
     }
 
